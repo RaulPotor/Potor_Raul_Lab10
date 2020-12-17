3aaee1f42ceb3eb1c360cf477178955ecc3fce81
RaulPotor <44375993+RaulPotor@users.noreply.github.com>
2020-12-17 14:21:39 +0200

    Add project files.

---
+++ Potor_Raul_Lab10/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Potor_Raul_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+}
